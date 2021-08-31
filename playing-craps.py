01dbdce3c849d9711402f86aafc6c391e4a95ed7
bmoser12 <89146471+bmoser12@users.noreply.github.com>
2021-08-31 14:00:52 -0500

    Update playing-craps.py

--- playing-craps.py
+++ playing-craps.py
@@ -51,3 +51,5 @@ print(f'{9:>4}{frequency9:>13}')
 print(f'{10:>4}{frequency10:>13}')
 print(f'{11:>4}{frequency11:>13}')
 print(f'{12:>4}{frequency12:>13}')
+
+print('Craps:',sum(frequency2,frequency3,frequency12)/6,000,000))
