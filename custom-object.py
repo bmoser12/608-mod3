3e7db4f43d7eda76bcdf650e787b768e7c466c45
bmoser12 <89146471+bmoser12@users.noreply.github.com>
2021-09-01 08:50:51 -0500

    Create custom-object.py

---
+++ custom-object.py
@@ -0,0 +1,19 @@
+#use this to figure out a bill total including tip and tax
+
+def tip_amount(total,tip_percent):
+    return total*tip_percent
+def tax_amount(total, tax_percent):
+    return total*tax_percent
+def bill_total(total,tax,tip):
+    return total+tip_amount+tax_amount
+    
+total = 100
+tip_percent = .20
+tax_percent = .075
+
+tax = tax_amount(total,tax_percent)
+tip = tip_amount(total,tip_percent)
+
+print("total tip:",tip)
+print('total tax:', tax)
+print('Bill total:',total+tax+tip)
